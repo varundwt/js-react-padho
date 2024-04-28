51259e9bd75d2b9eea0b4998d68d4eebf049df1f
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-04-28 20:04:27 +0530

    Create IIFE.js

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+//An immediately invoked function expression, or IIFE (pronounced iffy), is a function that is called immediately after it is defined. 
+
+// Use Cases Of IIFE :
+// Avoid polluting the global namespace.
+// To create closures in JavaScript.
+// IIFE is used to create private and  public variables and methods.
+// It is used to execute the async and await function.
+// It is used to work with require function.
+
+
+(function IIFE(){
+    console.log("I am an IIFE.");
+})();
